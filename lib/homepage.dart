4413c9cf4dbcaa6a722d6d373481d632253c52de
ronak <87222698+ronakgoti4499@users.noreply.github.com>
2022-05-14 19:24:22 +0530

    complet

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/materialain.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -10,54 +12,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List color = [
-    Colors.lightGreenAccent,
-    Colors.green,
-    Colors.deepPurple,
-    Colors.pinkAccent,
-    Colors.deepOrangeAccent,
-    Colors.brown,
+  final List<Model> color = [
+    Model(Colors.lightGreenAccent, 1),
+    Model(Colors.green, 2),
+    Model(Colors.deepPurple, 3),
+    Model(Colors.pinkAccent, 4),
+    Model(Colors.greenAccent, 5),
+    Model(Colors.deepPurple, 6),
+    Model(Colors.deepOrangeAccent, 7),
+    Model(Colors.blueGrey, 8),
+    Model(Colors.greenAccent, 9),
+    Model( Colors.purple, 10),
+    Model(Colors.red, 11),
+    Model(Colors.purpleAccent, 12),
   ];
 
-  final List color1 = [
-    Colors.lightBlueAccent,
-    Colors.blueGrey,
-    Colors.greenAccent,
-    Colors.purple,
-    Colors.red,
-    Colors.purpleAccent,
-  ];
+  // final List color1 = [
+  //   Colors.lightBlueAccent,
+  //   Colors.blueGrey,
+  //   Colors.greenAccent,
+  //   Colors.purple,
+  //   Colors.red,
+  //   Colors.purpleAccent,
+  // ];
 
-  int isIndex = 0;
+  // int index = 0;
 
-  String? choice;
+  Color? colorname;
 
-  Color? colors;
+  String? data;
 
-  int? value;
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    var preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      data = preferences.getString("color");
+      log("----------------------------------------------------------${data}");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: colorname,
+      ),
       body: Stack(
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 20, left: 30, right: 20),
-            child: ListView.builder(
-              itemCount: 6,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    item(color[index],()=> colors),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    item(color1[index],()=> colors),
-                  ],
+            padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+            child: GridView.count(
+              crossAxisCount: 6,
+              crossAxisSpacing: 5.0,
+              mainAxisSpacing: 5.0,
+              children: List<Widget>.generate(12, (index) {
+                return item(color[index].color, () async {
+                  setState(() {
+                    colorname = color[index].color;
+                  });
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.setString("color", "${color[index].id}");
+                  var p = prefs.getString("color");
+                  log("current drop == >${p}");
+                  log("-------${color[index].id}");
+                });
+                const SizedBox(
+                  height: 10,
+                  // item(color1[index], () {
+                  //   setState(() {
+                  //     colorname = color[index];
+                  //   });
+                  //   log("-------${colorname}");
+                  // }),
                 );
-              },
+              }),
             ),
           ),
           Padding(
@@ -65,9 +100,9 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Container(
               height: 80,
               width: 80,
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.purple,
+                color: colorname,
               ),
               child: const Icon(
                 Icons.account_circle,
@@ -81,24 +116,32 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget item(Color color , VoidCallback ontap) {
-    return Row(
-      children: [
-        GestureDetector(
-          onTap: ontap,
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: color,
-            ),
-            height: 40,
-            width: 40,
-          ),
+  Widget item( Color color, VoidCallback voidCallback) {
+    return
+        // MaterialButton(
+        //   onPressed: voidCallback,
+        //   color: color,
+        //   padding: EdgeInsets.zero,
+        //   shape: const CircleBorder(),
+        // ),
+
+        InkWell(
+      onTap: voidCallback,
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
         ),
-        SizedBox(
-          width: 10,
-        ),
-      ],
+        height: 30,
+        width: 30,
+      ),
     );
   }
+}
+
+class Model {
+  final Color color;
+  final int id;
+
+  Model(this.color, this.id);
 }
